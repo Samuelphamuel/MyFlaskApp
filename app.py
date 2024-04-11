ba2d8d2a0faab308d284cb2f77c3272f4ffff028
Sameul Pham <samuelphamuel@gmail.com>
2024-04-11 09:35:50 -0500

    Updated proper Python spacing and indents

--- app.py
+++ app.py
@@ -7,10 +7,12 @@ import pandas as pd
 
 # Initialize the Flask app
 app = Flask(__name__)
-app.config['SQLALCHEMY_DATABASE_URI'] = 'mysql://root:pa$$w0rd@localhost/storedata' # database user:password@localhost/database credentials
+# database user:password@localhost/database credentials
+app.config['SQLALCHEMY_DATABASE_URI'] = 'mysql://root:pa$$w0rd@localhost/storedata'
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 
 db = SQLAlchemy(app)
+
 
 # Define class Sales
 class Sales(db.Model):
@@ -19,17 +21,19 @@ class Sales(db.Model):
     # Table columns
     id = db.Column(db.Integer, primary_key=True)
     store_code = db.Column(db.String(5), nullable=False)
-    total_sale = db.Column(db.DECIMAL(9,2), nullable=False)
+    total_sale = db.Column(db.DECIMAL(9, 2), nullable=False)
     transaction_date = db.Column(db.Date, nullable=False)
 
     def __repr__(self):
         return '<Sales %r>' % self.id
+
 
 # Index route to display all data from database
 @app.route('/')
 def index():
     sales_data = Sales.query.all()
     return render_template('index.html', sales=sales_data)
+
 
 # Create route and function for POST request for creating new data in sales
 @app.route('/create', methods=['POST'])
@@ -38,19 +42,19 @@ def create_sale():
         id = int(request.form['id'])
         store_code = request.form['store_code'],
         total_sale = round(float(request.form['total_sale']), 2)
-        transaction_date = date.fromisoformat(request.form['transaction_date'])
+        transaction_date = datetime.fromisoformat(request.form['transaction_date'])
 
         new_sale = Sales(
-        id = id,
-        store_code = store_code,
-        total_sale = total_sale,
-        transaction_date = transaction_date
+            id=id,
+            store_code=store_code,
+            total_sale=total_sale,
+            transaction_date=transaction_date
         )
         try:
             db.session.add(new_sale)
             db.session.commit()
             return redirect('/')
-        except:
+        except ValueError:
             return 'Something went wrong'
 
     # Return to index.html with new created sale and order all sales ID
@@ -58,30 +62,33 @@ def create_sale():
         sales = Todo.query.order_by(Todo.id).all()
         return render_template('index.html', sales=sales)
 
+
 # Create route and function for PUT request for updating data
 @app.route('/update/<int:id>', methods=['GET', 'POST'])
 def update_sale(id):
-    sale = Sales.query.get_or_404(id) # Attempt to get that sale or 404 bad request
+    sale = Sales.query.get_or_404(id)  # Attempt to get that sale or 404 bad request
 
     if request.method == 'POST':
         sale.store_code = request.form['store_code']
         sale.total_sale = round(float(request.form['total_sale']), 2)
-        sale.transaction_date = date.fromisoformat(request.form['transaction_date'])
+        sale.transaction_date = datetime.fromisoformat(request.form['transaction_date'])
 
         db.session.commit()
         return redirect(url_for('index'))
 
-    return render_template('update.html', sale=sale) # Pass sale object to update.html
+    return render_template('update.html', sale=sale)  # Pass sale object to update.html
+
 
 # Create route and function for DELETE request for deleting data by ID
 @app.route('/delete/<int:id>', methods=['GET'])
 def delete_sale(id):
-    sale_to_delete = Sales.query.get_or_404(id) # Attempt to get that sale or 404 bad request
+    sale_to_delete = Sales.query.get_or_404(id)  # Attempt to get that sale or 404 bad request
 
     if sale_to_delete:
         db.session.delete(sale_to_delete)
         db.session.commit()
     return redirect(url_for('index'))
+
 
 # Route to execute SQL queries
 @app.route('/sql_query', methods=['POST'])
@@ -90,11 +97,11 @@ def sql_query():
     result = execute_sql_query(query)
     return jsonify(result)
 
+
 # GET endpoint to get data based on date range
 @app.route('/get_sales_data', methods=['GET'])
 def get_sales_data():
-
-    # Start and end date paremeters
+    # Start and end date parameters
     start_date = request.args.get('start_date')
     end_date = request.args.get('end_date')
 
@@ -115,17 +122,18 @@ def get_sales_data():
 
     # List
     list_data = [[sale.id,
-        sale.store_code,
-        float(sale.total_sale),
-        str(sale.transaction_date)] for sale in sales_data]
+                  sale.store_code,
+                  float(sale.total_sale),
+                  str(sale.transaction_date)] for sale in sales_data]
 
     # Pandas Data Frame
     df = pd.DataFrame(list_data, columns=['id', 'store_code', 'total_sale', 'transaction_date'])
 
     return jsonify({'json_data': json_data,
                     'list_data': list_data,
-                    'dataframe': df.to_dict(orient= 'records')
-    })
+                    'dataframe': df.to_dict(orient='records')
+                    })
+
 
 # POST endpoint to add new row to MySQL database
 @app.route('/add_row', methods=['POST'])
@@ -135,10 +143,10 @@ def add_row():
 
         # Extract parameters from request
         new_sale = Sales(
-            id = data['id'],
-            store_code = data['store_code'],
-            total_sales = data['total_sales'],
-            transaction_date = data['transaction_date']
+            id=data['id'],
+            store_code=data['store_code'],
+            total_sales=data['total_sales'],
+            transaction_date=data['transaction_date']
         )
 
         db.session.add(new_sale)
@@ -151,4 +159,3 @@ def add_row():
 # To run the Flask app
 if __name__ == '__main__':
     app.run(debug=True)
-
--- db_utils.py
+++ db_utils.py
@@ -1,14 +1,15 @@
 import pymysql.cursors
 
+
 # Function to connect to the MySQL database and return data
 def execute_sql_query(sql_query):
     # Connect to your database
     connection = pymysql.connect(
-        host = 'localhost', # your host name
-        user = 'root', # your username
-        password = 'pa$$w0rd', # your MySQL password
-        database = 'storedata', # your database name
-        cursorclass = pymysql.cursors.DictCursor
+        host='localhost',  # your host name
+        user='root',  # your username
+        password='pa$$w0rd',  # your MySQL password
+        database='storedata',  # your database name
+        cursorclass=pymysql.cursors.DictCursor
     )
 
     # Return all rows from executed query and execute queries by using cursor object
@@ -26,10 +27,10 @@ def execute_sql_query(sql_query):
 
     # Close cursor and connection
     finally:
-            connection.close()
+        connection.close()
+
 
 # Example SQL query
-query = "SELECT * FROM sales WHERE id = 5;" # Modify this SQL query to print result to terminal
+query = "SELECT * FROM sales WHERE id = 5;"  # Modify this SQL query to print result to terminal
 result = execute_sql_query(query)
 print(result)
-
